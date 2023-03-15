6c5c8d4c93952568f19807f273377a2ad1d5b3e1
tamanghawana0107 <hawanatamang@gmail.com>
2023-03-15 18:09:46 +0545

    Added

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using AdminRentalUI;
 using bikeRental.Petrol_Bike;
 using bikeRental.User_Controls;
 
@@ -14,7 +15,7 @@ namespace bikeRental
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Login());
+            Application.Run(new adminLoginUI());
         }
     }
 }
