51b15ed4b42db4a80e941fa675cb6a9dcb4ecf2f
tamanghawana0107 <hawanatamang@gmail.com>
2023-03-16 21:08:53 +0545

    Costura Fody related error solved

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace bikeRental
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Login());
+            Application.Run(new adminLoginUI());
         }
     }
 }
